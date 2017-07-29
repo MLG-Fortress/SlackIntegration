2cad550584014cb286987a4af3bee03cf46e713a
RoboMWM <robomwm@users.noreply.github.com>
2017-07-29 01:58:32 -0700

    nullcheck addon

--- src/main/java/com/teej107/slack/AsyncPlayerChatListener.java
+++ src/main/java/com/teej107/slack/AsyncPlayerChatListener.java
@@ -19,20 +19,22 @@ public class AsyncPlayerChatListener implements Listener
 		this.plugin = plugin;
 	}
 	GriefPrevention gp = (GriefPrevention)Bukkit.getPluginManager().getPlugin("GriefPrevention");
-	DataStore ds = gp.dataStore;
 
 	@EventHandler (priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onAsyncChat(AsyncPlayerChatEvent event)
 	{
-		//If a player is softmuted in GriefPrevention, ignore
-		if (ds.isSoftMuted(event.getPlayer().getUniqueId()))
-			return;
-
-		//If a player's spam is muted by GriefPrevention, ignore
-		if (Bukkit.getOnlinePlayers().size() > 1) //Impossible to tell if GP is filtering spam when only one player is on
-			if (event.getRecipients().size() < 2)
+		if (gp.dataStore != null)
+		{
+			//If a player is softmuted in GriefPrevention, ignore
+			if (gp.dataStore.isSoftMuted(event.getPlayer().getUniqueId()))
 				return;
 
+			//If a player's spam is muted by GriefPrevention, ignore
+			if (Bukkit.getOnlinePlayers().size() > 1) //Impossible to tell if GP is filtering spam when only one player is on
+				if (event.getRecipients().size() < 2)
+					return;
+		}
+		
 		plugin.sendToSlack(event.getPlayer(), event.getMessage());
 	}
 }
