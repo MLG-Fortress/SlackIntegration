240055141abe5aced0090bdc88c2593da26c245b
RoboMWM <robomwm@users.noreply.github.com>
2017-07-31 00:28:21 -0700

    respond to kick events

--- src/main/java/com/teej107/slack/ServerActivityListener.java
+++ src/main/java/com/teej107/slack/ServerActivityListener.java
@@ -1,11 +1,15 @@
 package com.teej107.slack;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 /**
@@ -41,12 +45,24 @@ public class ServerActivityListener implements Listener
         plugin.sendToSlack(SlackCommandSender.getInstance(), joinMessage);
     }
 
+    Set<Player> kickedPlayers = new HashSet<>();
+
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     private void onQuit(PlayerQuitEvent event)
     {
+        if (kickedPlayers.remove(event.getPlayer()))
+            return;
         String quitMessage = event.getQuitMessage();
         if (quitMessage == null || quitMessage.isEmpty())
             quitMessage = event.getPlayer().getName() + " left us in loneliness :c";
         plugin.sendToSlack(SlackCommandSender.getInstance(), quitMessage);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    private void onKick(PlayerKickEvent event)
+    {
+        String quitMessage = "Was kicked for " + event.getReason();
+        plugin.sendToSlack(SlackCommandSender.getInstance(), quitMessage);
+        kickedPlayers.add(event.getPlayer());
     }
 }
